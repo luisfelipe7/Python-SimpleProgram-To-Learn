aac862386aa998a6afba94f6a78196f7652f2b15
luisfelipe7 <luisfelipecs97@gmail.com>
2018-11-12 10:26:47 -0600

    Documentations and new folders added

---
+++ Python Documentation and Methods/Methods/Python Tuple Methods.py
@@ -0,0 +1,23 @@
+    Important Methods for Python Tuple Methods
+
+Python Tuple Methods:
+
+Python Tuple count() 	returns occurrences of element in a tuple
+Python Tuple index() 	returns smallest index of element in tuple
+Python any() 	Checks if any Element of an Iterable is True
+Python all() 	returns true when all elements in iterable is true
+Python ascii() 	Returns String Containing Printable Representation
+Python bool() 	Converts a Value to Boolean
+Python enumerate() 	Returns an Enumerate Object
+Python filter() 	constructs iterator from elements which are true
+Python iter() 	returns iterator for an object
+Python len() 	Returns Length of an Object
+Python max() 	returns largest element
+Python min() 	returns smallest element
+Python map() 	Applies Function and Returns a List
+Python reversed() 	returns reversed iterator of a sequence
+Python slice() 	creates a slice object specified by range()
+Python sorted() 	returns sorted list from a given iterable
+Python sum() 	Add items of an Iterable
+Python tuple() Function 	Creates a Tuple
+Python zip() 	Returns an Iterator of Tuples
---
+++ Python Documentation and Methods/Python Resources.py
@@ -0,0 +1,4 @@
+    Sites with resources
+
+1. https: // www.programiz.com/python-programming/methods/built-in
+2. https: // docs.python.org/3/genindex-all.html
